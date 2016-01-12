e64fc62471cde46472587f24b7aa4bb6d97d0704
pukkamustard <adarsh@amirtham.ch>
2016-01-12 17:39:50 +0100

    basic working service.

--- service/app/index.js
+++ service/app/index.js
@@ -3,17 +3,37 @@ var app = express();
 
 var s3 = require('./s3');
 
-app.get('/', function(req, res) {
-    res.send('Hello World!');
-});
-
-// app.listen(3000, function() {
-//     console.log('Example app listening on port 3000!');
-// })
-
-
 var db;
 s3.getDb().then(function(b) {
     db = b;
-    console.log(db)
+})
+
+
+app.get('/', function(req, res) {
+    res.send('Hello my name is Pastan!');
+});
+
+
+app.get('/items', function(req, res, next) {
+    res.json(db.items)
+})
+
+app.get('/items/:id', function(req, res, next) {
+    var id = req.params.id;
+    res.json(db.items[id])
+})
+
+app.get('/items/:id/url', function(req, res, next) {
+    var id = req.params.id;
+    var item = db.items[id];
+    return s3.getUrl(item)
+        .then(function(url) {
+            res.json({
+                url: url
+            })
+        });
+})
+
+app.listen(8338, function() {
+    console.log('Pastan listening on port 8338!');
 })
