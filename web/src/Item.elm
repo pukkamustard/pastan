580b835236d89e01d9fec2bd1c192f03cf9ccee9
pukkamustard <adarsh@amirtham.ch>
2016-03-02 23:29:13 +0100

    Fix item compare

--- web/src/Item.elm
+++ web/src/Item.elm
@@ -11,13 +11,24 @@ type alias Item =
   , length : Float
   , album : String
   , album_id : Int
+  , albumartist : String
   }
 
 
 decode : Json.Decoder Item
 decode =
-  Json.object7
-    (\id track artist title length album album_id -> { id = id, track = track, artist = artist, title = title, length = length, album = album, album_id = album_id })
+  Json.object8
+    (\id track artist title length album album_id albumartist ->
+      { id = id
+      , track = track
+      , artist = artist
+      , title = title
+      , length = length
+      , album = album
+      , album_id = album_id
+      , albumartist = albumartist
+      }
+    )
     ("id" := Json.int)
     ("track" := Json.int)
     ("artist" := Json.string)
@@ -25,6 +36,7 @@ decode =
     ("length" := Json.float)
     ("album" := Json.string)
     ("album_id" := Json.int)
+    ("albumartist" := Json.string)
 
 
 decodeList : Json.Decoder (List Item)
@@ -39,19 +51,39 @@ fileUrl i =
 
 compareItem : Item -> Item -> Order
 compareItem a b =
-  compareRecord [ .artist, (toString << .album_id), (toString << .track), .title] a b
-
-
-compareRecord : List (a -> comparable) -> a -> a -> Order
-compareRecord fields a b =
   let
-    ca =
-      List.map (\f -> f a) fields
-
-    cb =
-      List.map (\f -> f b) fields
+    c =
+      [ compareStringField .albumartist a b
+      , compareIntField .album_id a b
+      , compareIntField .track a b
+      , compareStringField .title a b
+      , compareIntField .id a b
+      ]
   in
-    List.foldr difference EQ (List.map2 compare ca cb)
+    List.foldr difference EQ c
+
+
+compareStringField : (Item -> String) -> Item -> Item -> Order
+compareStringField field a b =
+  compare (field a) (field b)
+
+
+compareIntField : (Item -> Int) -> Item -> Item -> Order
+compareIntField field a b =
+  compare (field a) (field b)
+
+
+
+-- compareRecord : List (a -> comparable) -> a -> a -> Order
+-- compareRecord fields a b =
+--   let
+--     ca =
+--       List.map (\f -> f a) fields
+--
+--     cb =
+--       List.map (\f -> f b) fields
+--   in
+--     List.foldr difference EQ (List.map2 compare ca cb)
 
 
 difference : Order -> Order -> Order
